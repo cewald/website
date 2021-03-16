1c3d8321a957a57b97bad2c35559494f08b7b277
cewald <c.ewald@impericon.com>
2021-03-16 15:26:59 +0100

    Add logo

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,14 @@
   <div
     class="container min-h-screen mx-auto flex flex-col justify-center px-4 py-10 font-mono text-base-darkest"
   >
-    <div class="mb-10">
-      <h1 class="text-2xl font-bold">{{ headline }}</h1>
-      <p class="font-thin text-lg">
-        {{ metaLine }}
-      </p>
+    <div class="flex flex-wrap items-center mb-10">
+      <logo class="mb-4 md:mr-4 md:mb-0" />
+      <div>
+        <h1 class="text-2xl font-bold">{{ headline }}</h1>
+        <p class="font-thin text-lg">
+          {{ metaLine }}
+        </p>
+      </div>
     </div>
     <skill-set class="mb-10">Skillset:</skill-set>
     <contact>Get in touch:</contact>
