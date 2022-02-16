541986895eb209b10e549c420d5dc0b9cbed64c0
cewald <hello@christianewald.de>
2022-02-16 18:05:58 +0100

    Update GID

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -4,7 +4,7 @@ import VueGtag from 'vue-gtag-next'
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: '303834268'
+      id: 'G-31PMQRSJNE'
     }
   })
 })
