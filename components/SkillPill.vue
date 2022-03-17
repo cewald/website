e66d0a2850d1c993ff255e86a309c647fbea031f
cewald <hello@christianewald.de>
2022-03-17 18:50:15 +0100

    Add hire me badge

--- components/SkillPill.vue
+++ components/SkillPill.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="inline-block my-2 mr-4 px-2 py-1 bg-base-lightest rounded cursor-pointer hover:bg-base-darkest hover:text-white"
+    class="inline-block my-2 mr-4 px-2 py-1 bg-base-lightest rounded hover:bg-base-darkest hover:text-white"
   >
     <slot />
   </span>
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,16 @@
   >
     <div class="flex flex-wrap items-center mb-10">
       <logo class="mb-4 md:mr-4 md:mb-0" />
-      <div>
-        <h1 class="text-2xl font-bold">{{ headline }}</h1>
-        <p class="font-thin text-lg" v-text="metaLine" />
+      <div class="flex flex-wrap items-center">
+        <h1 class="flex-initial text-2xl font-bold order-1">{{ headline }}</h1>
+        <p class="font-thin text-lg w-full order-3" v-text="metaLine" />
+        <a
+          href="https://www.linkedin.com/in/ewaldtm"
+          title="I'm currently available"
+          class="order-2 ml-2 text-xs text-red-700 uppercase"
+        >
+         <span class="text-base-light"> | </span> Hire me
+        </a>
       </div>
     </div>
     <skill-set class="mb-10">Skillset:</skill-set>
