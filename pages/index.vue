95f1f483be75acd7c235442eb33b40e12eb7c729
cewald <hello@christianewald.de>
2022-11-03 17:43:46 +0000

    Variuous changes [WIP]

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,13 @@
 <template>
-  <div
-    class="container min-h-screen mx-auto flex flex-col justify-center px-4 py-10 font-mono text-base-darkest"
-  >
+  <div class="container min-h-screen mx-auto flex flex-col justify-center px-4 py-10 font-mono text-base-darkest">
     <div class="flex flex-wrap items-center mb-10">
       <logo class="mb-4 md:mr-4 md:mb-0" />
       <div class="flex flex-wrap items-center">
         <h1 class="flex-initial text-2xl font-bold order-1">{{ headline }}</h1>
-        <p class="font-thin text-lg w-full order-3" v-text="metaLine" />
-        <a
-          href="https://www.linkedin.com/in/ewaldtm"
-          title="I'm currently available"
-          class="order-2 ml-2 text-xs text-red-700 uppercase"
-        >
-         <span class="text-base-light"> | </span> Hire me
+        <p class="font-thin text-lg      w-full order-3" v-text="metaLine" />
+        <a href="https://www.linkedin.com/in/ewaldtm" title="I'm currently available"
+          class="order-2 ml-2 text-xs text-red-700 uppercase">
+          <span class="text-base-light"> | </span> Hire me
         </a>
       </div>
     </div>
