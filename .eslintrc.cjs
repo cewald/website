0d3527338367693f2cb4c01e6d719d33b6ffbd87
cewald <hello@christianewald.de>
2022-11-04 07:57:34 +0000

    Bugfix for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,9 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@nuxtjs/eslint-config-typescript',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier'
   ],
