dd3d480d15bfa89b00498ec4b709f4fbadd4bd1e
cewald <hello@christianewald.de>
2023-12-03 13:49:47 +0100

    Add `pinia` and `tailwindCSS`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  srcDir: 'src',
+  ssr: true,
+  css: ['@/styles/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@pinia/nuxt'],
+  devtools: { enabled: true },
 })
