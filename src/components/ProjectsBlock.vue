675f5ca1109616db8653165dd8fa45a862c700dd
cewald <hello@christianewald.de>
2024-04-16 12:12:48 +0200

    Improve underline on desktop

--- src/components/ProjectsBlock.vue
+++ src/components/ProjectsBlock.vue
@@ -39,7 +39,7 @@ const projects: {
           <a
             :href="link"
             target="_blank"
-            class="underline decoration-base-light underline-offset-8 hover:decoration-black"
+            class="underline decoration-base-light underline-offset-8 hover:decoration-black md:decoration-1"
           >
             {{ title }}
           </a>
