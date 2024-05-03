d35e77a6b27097a6b3751326219a490cabea8797
cewald <hello@christianewald.de>
2024-05-03 08:10:23 +0000

    Switch skill-gant to composition API

--- src/components/cv/SkillGantt.vue
+++ src/components/cv/SkillGantt.vue
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang="ts" setup>
 import Skillset from '~/data/skillset.json'
 
 type SkillSetSkill = {
@@ -18,112 +18,104 @@ type SkillSetSection = {
   skills: SkillSetSkill[]
 }
 
-export default {
-  setup() {
-    const skillset = ref(Skillset as SkillSetSection[])
-    const endDate = ref(new Date(new Date().getFullYear().toString()))
+const skillset = ref(Skillset as SkillSetSection[])
+const endDate = ref(new Date(new Date().getFullYear().toString()))
 
-    return {
-      skillset,
-      endDate,
-    }
-  },
-  computed: {
-    skillsetStruct() {
-      return ([ ...this.skillset ]).map(section => {
-        section.skills.map(skill => {
-          // Map year as JS date
-          skill.timestampedTimeslots = skill.timeslots.map(s => {
-            // eslint-disable-next-line prefer-const
-            let [ start, stop ] = s.split('-').map(v => new Date(v))
-            if (start.getFullYear() < this.startDate.getFullYear()) {
-              start = this.startDate
-            }
-
-            return { start, stop }
-          })
-
-          skill.percentTimeslots = skill.timestampedTimeslots
-            // Map start/stop time in %
-            .map(({ start, stop }) => ({
-              start: this.timeStampToPercent(start),
-              stop: stop ? this.timeStampToPercent(stop) : undefined,
-            }))
-            // Map bar-width in %
-            .map(({ start, stop }) => {
-              const width: number
-                = stop && stop > 0 ? stop - start : 100 - start
-              return { start, stop, width }
-            })
-            // Reset start point if slot has prepending slots
-            .map((s, i, arr) => {
-              if (i > 0) {
-                const { width: w } = arr[i - 1]
-                s.start = s.start + w
-              }
-              return s
-            })
-
-          return skill
-        })
-        return section
-      })
-    },
-    startDate() {
-      const slots: number[] = []
-      this.skillset.forEach(section => {
-        for (const skill of section.skills) {
-          const startSlots = skill.timeslots.map(
-            s => s.split('-').map(v => parseInt(v))[0],
-          )
-
-          slots.push(...startSlots)
+const skillsetStruct = computed(() => {
+  return ([ ...skillset.value ]).map(section => {
+    section.skills.map(skill => {
+      // Map year as JS date
+      skill.timestampedTimeslots = skill.timeslots.map(s => {
+        // eslint-disable-next-line prefer-const
+        let [ start, stop ] = s.split('-').map(v => new Date(v))
+        if (start.getFullYear() < startDate.value.getFullYear()) {
+          start = startDate.value
         }
+
+        return { start, stop }
       })
 
-      const firstYear = slots.reduce((a, b) => (a > b ? b : a))
-
-      const endDate = this.endDate.getFullYear()
-      if (firstYear < endDate - 12) {
-        return new Date((endDate - 12).toString())
-      }
-
-      return new Date(firstYear.toString())
-    },
-    yearScale() {
-      return this.getRange(
-        this.endDate.getFullYear(),
-        this.startDate.getFullYear(),
-        4,
-      )
-        .map(y => Math.round(y))
-        .map(year => ({
-          year,
-          percent: this.timeStampToPercent(new Date(year.toString())),
+      skill.percentTimeslots = skill.timestampedTimeslots
+      // Map start/stop time in %
+        .map(({ start, stop }) => ({
+          start: timeStampToPercent(start),
+          stop: stop ? timeStampToPercent(stop) : undefined,
         }))
-        .reverse()
-    },
-  },
-  methods: {
-    timeStampToPercent(timestamp: Date): number {
-      const start = this.startDate.getTime()
-      const end = this.endDate.getTime()
-      const scale = end - start
-      const point = timestamp.getTime() - start
-      return Math.round((point * 100) / scale)
-    },
-    getRange(upper: number, lower: number, steps: number) {
-      const difference = upper - lower
-      const increment = difference / (steps - 1)
-      return [
-        lower,
-        ...Array(steps - 2)
-          .fill('')
-          .map((_, index) => lower + increment * (index + 1)),
-        upper,
-      ]
-    },
-  },
+      // Map bar-width in %
+        .map(({ start, stop }) => {
+          const width: number
+                = stop && stop > 0 ? stop - start : 100 - start
+          return { start, stop, width }
+        })
+      // Reset start point if slot has prepending slots
+        .map((s, i, arr) => {
+          if (i > 0) {
+            const { width: w } = arr[i - 1]
+            s.start = s.start + w
+          }
+          return s
+        })
+
+      return skill
+    })
+    return section
+  })
+})
+
+const startDate = computed(() => {
+  const slots: number[] = []
+  skillset.value.forEach(section => {
+    for (const skill of section.skills) {
+      const startSlots = skill.timeslots.map(
+        s => s.split('-').map(v => parseInt(v))[0],
+      )
+
+      slots.push(...startSlots)
+    }
+  })
+
+  const firstYear = slots.reduce((a, b) => (a > b ? b : a))
+
+  const endDateYear = endDate.value.getFullYear()
+  if (firstYear < endDateYear - 12) {
+    return new Date((endDateYear - 12).toString())
+  }
+
+  return new Date(firstYear.toString())
+})
+
+const yearScale = computed(() => {
+  return getRange(
+    endDate.value.getFullYear(),
+    startDate.value.getFullYear(),
+    4,
+  )
+    .map(y => Math.round(y))
+    .map(year => ({
+      year,
+      percent: timeStampToPercent(new Date(year.toString())),
+    }))
+    .reverse()
+})
+
+const timeStampToPercent = (timestamp: Date): number => {
+  const start = startDate.value.getTime()
+  const end = endDate.value.getTime()
+  const scale = end - start
+  const point = timestamp.getTime() - start
+  return Math.round((point * 100) / scale)
+}
+
+const getRange = (upper: number, lower: number, steps: number) => {
+  const difference = upper - lower
+  const increment = difference / (steps - 1)
+  return [
+    lower,
+    ...Array(steps - 2)
+      .fill('')
+      .map((_, index) => lower + increment * (index + 1)),
+    upper,
+  ]
 }
 </script>
 
