843861faec1be100a85156ca7c4b91962fb51e3b
cewald <hello@christianewald.de>
2024-05-03 07:50:03 +0200

    Add cv link to contact-block

--- src/components/ButtonPill.vue
+++ src/components/ButtonPill.vue
@@ -1,14 +1,19 @@
 <script lang="ts" setup>
-defineProps<{
+const { to } = defineProps<{
   title: string
   to: string
   transparent?: boolean
 }>()
+
+const isExternal = computed(() => to.startsWith('http') || to.startsWith('mailto') || to.startsWith('tel'))
+const component = computed(() => isExternal.value ? 'a' : 'RouterLink')
+const link = computed(() => isExternal.value ? { href: to } : { to })
 </script>
 
 <template>
-  <a
-    :href="to"
+  <component
+    :is="component"
+    v-bind="{ ...link }"
     class="flex items-center px-4 py-2 text-lg"
     :class="{
       'bg-base-darkest text-white dark:bg-slate-900': !transparent,
@@ -18,5 +23,5 @@ defineProps<{
   >
     <slot />
     <span v-text="title" />
-  </a>
+  </component>
 </template>
