791aba5300f5d8cbfd72bb21603375b0b74c7ced
cewald <c.ewald@spiri.bo>
2024-08-28 22:26:23 +0200

    feat: add conventional-commits and commit-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,5 @@
-import stylistic from '@stylistic/eslint-plugin'
-import pluginTailwindCSS from 'eslint-plugin-tailwindcss'
+import config from '@cewald/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt([
-  ...pluginTailwindCSS.configs['flat/recommended'],
-  {
-    plugins: { '@stylistic': stylistic },
-    rules: {
-      '@stylistic/semi': [ 2, 'never' ],
-      '@stylistic/indent': [ 'error', 2 ],
-      '@stylistic/max-len': [ 'error', { code: 120 } ],
-      '@stylistic/object-curly-spacing': [ 'error', 'always' ],
-      '@stylistic/quotes': [ 2, 'single', { avoidEscape: true, allowTemplateLiterals: false } ],
-      '@stylistic/arrow-parens': [ 'error', 'as-needed' ],
-      '@stylistic/eol-last': [ 'error', 'always' ],
-      '@stylistic/no-trailing-spaces': [ 'error' ],
-      '@stylistic/brace-style': [ 'error', '1tbs', { allowSingleLine: true } ],
-      '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
-    },
-  },
-])
+const corporateConfig = config({ tailwindcss: true })
+export default withNuxt(corporateConfig)
